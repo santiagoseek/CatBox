78f4406b0e842b414589c2445e685ac373081e21
testIniMac <alex-zhao76@hotmail.com>
2016-02-01 20:09:21 +0800

    Revert "Add DataStorageUtil and ToastUtil"
    This reverts commit 9fd2fa74bcfebe620984ec4be0471e3790f176c7.

--- app/src/main/java/com/santiago/catbox/activity/MainActivity.java
+++ app/src/main/java/com/santiago/catbox/activity/MainActivity.java
@@ -36,10 +36,8 @@ import com.santiago.catbox.NDK.TestNDK;
 import com.santiago.catbox.R;
 import com.santiago.catbox.common.Constant;
 import com.santiago.catbox.util.ConnectionIPWeight;
-import com.santiago.catbox.util.DataStorageUtil;
 import com.santiago.catbox.util.NetworkUtil;
 import com.santiago.catbox.util.SystemInfo;
-import com.santiago.catbox.util.ToastUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -125,11 +123,6 @@ public class MainActivity extends AppCompatActivity {
 					}
 					case 7: {
 						showNotification("Test","This is Testing for Notification.");
-						break;
-					}
-					case 8: { //StorageTest
-						ToastUtil.showToast(context, DataStorageUtil.getInternalFilePath(context),Toast.LENGTH_LONG);
-						ToastUtil.showToast(context, DataStorageUtil.getExternalStoragePath(context),Toast.LENGTH_LONG);
 						break;
 					}
 				}
@@ -251,7 +244,7 @@ public class MainActivity extends AppCompatActivity {
 			items.add("NDK");
 			items.add("WebView");//6
 			items.add("showNotification");
-			items.add("StorageTest");
+			items.add("SendTrace");
 			items.add("test");
 			items.add("DisplayInfo");
 			items.add("MultiProcessServiceStart");//11
